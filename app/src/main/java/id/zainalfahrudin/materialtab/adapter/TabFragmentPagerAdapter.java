d32f6d12eb234af1982c87086b2161ddd0b48b6f
Zainal Fahrudin <fnzainal@gmail.com>
2016-10-16 22:21:37 +0700

    first commit

---
+++ app/src/main/java/id/zainalfahrudin/materialtab/adapter/TabFragmentPagerAdapter.java
@@ -0,0 +1,51 @@
+package id.zainalfahrudin.materialtab.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import id.zainalfahrudin.materialtab.fragment.Tab1Fragment;
+import id.zainalfahrudin.materialtab.fragment.Tab2Fragment;
+
+/**
+ * Created by zainal on 10/16/16.
+ */
+
+public class TabFragmentPagerAdapter extends FragmentPagerAdapter {
+    //nama tab nya
+    String[] title = new String[]{
+            "Tab 1", "Tab 2"
+    };
+
+    public TabFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    //method ini yang akan memanipulasi penampilan Fragment dilayar
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new Tab1Fragment();
+                break;
+            case 1:
+                fragment = new Tab2Fragment();
+                break;
+            default:
+                fragment = null;
+                break;
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return title[position];
+    }
+
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+}
